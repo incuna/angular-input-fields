2cb1ba73d462d8843a3967fa97c450d9141326cd
Beth Menzies <beth@incuna.com>
2017-04-20 16:06:23 +0100

    remove blur focus stuff that doesn't work

--- src/select-input/templates.js
+++ src/select-input/templates.js
@@ -2,7 +2,7 @@ angular.module('aif-select-input').run(['$templateCache', function($templateCach
   'use strict';
 
   $templateCache.put('templates/aif/select-input.html',
-    "<div class=select-wrapper><select class=select ng-model=model ng-disabled=disable id=\"{{ fieldId }}\" name=\"{{ fieldId }}\" ng-focus=\"form.fieldId.hasFocus = true\" ng-blur=\"form.fieldId.hasFocus = false\" ng-options=\"choice.value as choice.display_name for choice in choices\"><option value=\"\" ng-if=emptyLabel ng-bind=emptyLabel></option></select></div>"
+    "<div class=select-wrapper><select class=select ng-model=model ng-disabled=disable id=\"{{ fieldId }}\" name=\"{{ fieldId }}\" ng-options=\"choice.value as choice.display_name for choice in choices\"><option value=\"\" ng-if=emptyLabel ng-bind=emptyLabel></option></select></div>"
   );
 
 }]);
